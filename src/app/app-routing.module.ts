1153e3cceea3c6e467b5f35e3775eff52a2381df
DuvanR11 <duriveramo@gmail.com>
2022-10-04 17:57:15 -0500

    Subida a github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes enrutados
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'contactanos', component: ContactosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ContactosComponent } from './pages/contactos/contactos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Modulos Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent,
+    FooterComponent,
+    PartnersComponent,
+    ClientesComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
